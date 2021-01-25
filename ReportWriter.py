d39d882854673ac12f4a51c55a5810d3e371d2eb
seccode <sec288@cornell.edu>
2021-01-25 04:05:38 -0500

    new report template

--- ReportWriter.py
+++ ReportWriter.py
@@ -11,7 +11,7 @@ ADDRESS = "1911 Sowles Park Drive, Katy, TX, USA"
 COMPANY = "NY2 Enterprises"
 CONTACT = "frank@ny2enterprises.com"
 
-ADDRESS_FONT = ImageFont.truetype("fonts/Lucida Grande.ttf", 42)
+ADDRESS_FONT = ImageFont.truetype("fonts/Lucida Grande.ttf", 36)
 DATE_FONT = ImageFont.truetype("fonts/Lucida Grande.ttf", 32)
 CONTACT_1_FONT = ImageFont.truetype("fonts/Lucida Grande.ttf", 32)
 CONTACT_2_FONT = ImageFont.truetype("fonts/Lucida Grande.ttf", 20)
@@ -80,13 +80,14 @@ class ReportWriter:
     def add_header_to_img(self, img):
         draw = ImageDraw.Draw(img)
 
-        draw.text((125, 170), self.address, (255,255,255),
+        w, h = ADDRESS_FONT.getsize(self.address)
+        draw.text((1100 - w, 164), self.address, (255,255,255),
                   font=ADDRESS_FONT)
 
         _date = date.today().strftime("%B %d, %Y")
 
         w, h = DATE_FONT.getsize(_date)
-        draw.text((1150 - w, 95), _date, (100,100,100),
+        draw.text((1110 - w, 95), _date, (207,207,207),
                   font=DATE_FONT)
 
         return img
@@ -99,46 +100,45 @@ class ReportWriter:
         draw = ImageDraw.Draw(page)
 
         def fill_contact_box():
-            draw_underlined_text(draw, (180, 1180), "Company:",
+            draw_underlined_text(draw, (180, 1130), "Company:",
                                 CONTACT_1_FONT, (0,0,0))
 
-            draw_underlined_text(draw, (180, 1230), "Contact:",
+            draw_underlined_text(draw, (180, 1180), "Contact:",
                                 CONTACT_1_FONT, (0,0,0))
 
-            draw.text((350, 1188), self.company, (0,0,0), font=CONTACT_2_FONT)
+            draw.text((350, 1138), self.company, (0,0,0), font=CONTACT_2_FONT)
 
-            draw.text((320, 1237), self.contact, (0,0,0), font=CONTACT_2_FONT)
+            draw.text((320, 1187), self.contact, (0,0,0), font=CONTACT_2_FONT)
 
         def fill_key_measurements():
-            i = 0
-            y_pos = 810
+            x_pos, y_pos = 905, 778
+
             for i in range(8):
                 if i == 0:
                     text = '{0:,.0f}'.format(self.measurements[i]) + " sq. ft."
-                    draw.text((940, y_pos), text, (0,0,0),
+                    draw.text((x_pos, y_pos), text, (0,0,0),
                             font=MEASUREMENTS_FONT)
 
                 elif i > 2:
                     text = '{0:,}'.format(self.measurements[i]) + " ft."
-                    draw.text((940, y_pos), text, (0,0,0),
+                    draw.text((x_pos, y_pos), text, (0,0,0),
                             font=MEASUREMENTS_FONT)
 
                 else:
                     text = str(self.measurements[i])
-                    draw.text((940, y_pos), text, (0,0,0),
+                    draw.text((x_pos, y_pos), text, (0,0,0),
                             font=MEASUREMENTS_FONT)
 
 
-                if i % 2 == 0:
-                    y_pos += 31
-                else:
-                    y_pos += 32
+                y_pos += 30
+                if i % 4 == 0:
+                    y_pos += 1
 
         def add_image():
-            box_left = 200
-            box_right = 650
-            box_top = 375
-            box_bottom = 1080
+            box_left = 180
+            box_right = 640
+            box_top = 370
+            box_bottom = 1040
 
             img = Image.open(os.path.join(self.folder, "top.png"))
 
@@ -160,7 +160,7 @@ class ReportWriter:
         page = self.add_header_to_img(page)
 
         def add_image():
-            box_left = 160
+            box_left = 155
             box_right = 1110
             box_top = 385
             box_bottom = 1350
@@ -184,7 +184,7 @@ class ReportWriter:
 
         draw = ImageDraw.Draw(page)
 
-        draw.text((995, 370),
+        draw.text((952, 356),
                   '{0:,.0f}'.format(self.measurements[0]) + " sq. ft.",
                   (0,0,0), font=AREA_FONT)
 
@@ -214,8 +214,6 @@ class ReportWriter:
         draw = ImageDraw.Draw(page)
 
         def fill_length_table():
-            y_pos = 393
-
             COLORS = [
                 (255,34,0),
                 (255,136,0),
@@ -224,23 +222,22 @@ class ReportWriter:
                 (72,72,72)
             ]
 
+            y_pos = 376
+
             for i in range(3,8):
                 text = '{0:,}'.format(self.measurements[i])
                 w, h = LENGTHS_FONT.getsize(text)
 
-                draw.text((1070 - w / 2, y_pos), text,
+                draw.text((1026 - w / 2, y_pos), text,
                     COLORS[i-3], font=LENGTHS_FONT)
 
-                if i % 2 == 0:
-                    y_pos += 39
-                else:
-                    y_pos += 40
+                y_pos += 38
 
         def add_image():
-            box_left = 160
+            box_left = 105
             box_right = 1110
-            box_top = 600
-            box_bottom = 1350
+            box_top = 570
+            box_bottom = 1310
 
             img = Image.open(os.path.join(self.folder, "Length.png"))
 
@@ -333,10 +330,10 @@ class ReportWriter:
             return
 
         def add_image():
-            box_left = 160
-            box_right = 1110
-            box_top = 375
-            box_bottom = 1360
+            box_left = 155
+            box_right = 1060
+            box_top = 355
+            box_bottom = 1380
 
             img = Image.open(os.path.join(self.folder, "views.png"))
 
@@ -359,10 +356,10 @@ class ReportWriter:
         draw = ImageDraw.Draw(page)
 
         def fill_pitch_and_area_table():
-            table_left = 375
-            table_right = 1120
-            table_top = 462
-            table_bottom = 553
+            table_left = 350
+            table_right = 1080
+            table_top = 442
+            table_bottom = 530
 
             cols = len(self.measurements[8])
 
@@ -383,39 +380,34 @@ class ReportWriter:
                 area_text = '{0:,.0f}'.format(self.measurements[8][pitch])
                 percent_text = str(int(100 * self.measurements[8][pitch] / self.measurements[0])) + "%"
 
-                draw.text((x_pos - SUMMARY_FONT.getsize(pitch_text)[0] / 2, 469), pitch_text, (0,0,0), font=SUMMARY_FONT)
-                draw.text((x_pos - SUMMARY_FONT.getsize(area_text)[0] / 2, 500), area_text, (0,0,0), font=SUMMARY_FONT)
-                draw.text((x_pos - SUMMARY_FONT.getsize(percent_text)[0] / 2, 531), percent_text, (0,0,0), font=SUMMARY_FONT)
+                draw.text((x_pos - SUMMARY_FONT.getsize(pitch_text)[0] / 2, 449), pitch_text, (0,0,0), font=SUMMARY_FONT)
+                draw.text((x_pos - SUMMARY_FONT.getsize(area_text)[0] / 2, 479), area_text, (0,0,0), font=SUMMARY_FONT)
+                draw.text((x_pos - SUMMARY_FONT.getsize(percent_text)[0] / 2, 509), percent_text, (0,0,0), font=SUMMARY_FONT)
 
             return
 
         def fill_waste_table():
-            table_left = 283
-            table_right = 1125
-            table_top = 818
-            table_bottom = 755
-
             area = self.measurements[0]
 
-            x_pos = 344
-            spacing = 120
+            x_pos = 330
+            spacing = 115
 
             for waste_percentage in WASTE_PERCENTAGES:
                 curr_area = area * waste_percentage
                 area_text = '{0:,.0f}'.format(curr_area)
                 square_text = '{0:.1f}'.format(curr_area / 100)
 
-                draw.text((x_pos - SUMMARY_FONT.getsize(area_text)[0] / 2, 763), area_text, (0,0,0), font=SUMMARY_FONT)
-                draw.text((x_pos - SUMMARY_FONT.getsize(square_text)[0] / 2, 794), square_text, (0,0,0), font=SUMMARY_FONT)
+                draw.text((x_pos - SUMMARY_FONT.getsize(area_text)[0] / 2, 731), area_text, (0,0,0), font=SUMMARY_FONT)
+                draw.text((x_pos - SUMMARY_FONT.getsize(square_text)[0] / 2, 762), square_text, (0,0,0), font=SUMMARY_FONT)
 
                 x_pos += spacing
 
             return
 
         def fill_length_table():
-            x_pos = 408
-            y_pos = 1040
-            x_spacing = 159
+            x_pos = 391
+            y_pos = 997
+            x_spacing = 153
 
             for i in range(3,8):
                 length_text = '{0:,}'.format(self.measurements[i])
@@ -447,7 +439,7 @@ class ReportWriter:
             self.make_page_6()
         ]
 
-        cover_img.save(filename, "PDF", resolution=100.0,
+        cover_img.save(filename, "PDF", resolution=200.0,
                        save_all=True, append_images=page_imgs)
 
         return
